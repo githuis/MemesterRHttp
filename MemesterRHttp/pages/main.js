366dd15ee7292d8bdda598435df61fbee09acb42
teamtunfiske <jbaa14@student.aau.dk>
2016-11-16 19:25:46 +0100

    lækkerhed

--- MemesterRHttp/pages/main.js
+++ MemesterRHttp/pages/main.js
@@ -5,6 +5,8 @@ var video = $video.get(0);
 var $playButton = $('.play');
 var $fullscreen = $('.fullscreen');
 
+
+
 $(".play").click(function () {
     if (video.paused) {
         video.play()
@@ -22,13 +24,22 @@ $container.find('.fullscreen').click(function () {
 
 
 var $progress = $('.progress');
+$progress.val(0);
+var changing = false;
 
 $progress.change(function (e) {
-    console.log("asd");
+    changing = true;
     var progress = (video.duration / 100) * $(this).val();
     video.currentTime = progress;
+    changing = false;
 });
- 
+
 $video.on('timeupdate', function () {
+    if(changing == false)
     $progress.val((video.currentTime * 100) / video.duration)
 });
+
+$video.on('ended', function () {
+    $playButton.toggleClass('fa-pause');
+});
+
