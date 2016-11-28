c58f477a1810754aedda7b6f8c8a963b380755e7
teamtunfiske <jbaa14@student.aau.dk>
2016-11-28 23:08:12 +0100

    ads

--- MemesterRHttp/pages/ecs/thr/thread.js
+++ MemesterRHttp/pages/ecs/thr/thread.js
@@ -1,9 +1,9 @@
 var meme = document.getElementById("template");
 var $content = $("#content");
 
-function addMeme(title,id) {
+function addMeme(id) {
     var clone = meme.content.cloneNode(true);
-  //  clone.querySelector('img').src = "/thumbs/" + id + ".png";
+   clone.querySelector('img').src = "/thumbs/" + id + ".png";
     clone.querySelector('img').onclick = function () {
         window.location.href = "/memes/" + id;
     };
@@ -11,16 +11,12 @@ function addMeme(title,id) {
     $content.append(clone);
 }
 
-addMeme("hej jens", "ldskjnfasd34209p");
-addMeme("hej jens", "ldskjnfasd34209p");
-addMeme("hej jens", "ldskjnfasd34209p");
-addMeme("hej jens", "ldskjnfasd34209p");
-addMeme("hej jens", "ldskjnfasd34209p");
-addMeme("hej jens", "ldskjnfasd34209p");
-addMeme("hej jens", "ldskjnfasd34209p");
-addMeme("hej jens", "ldskjnfasd34209p");
-addMeme("hej jens", "ldskjnfasd34209p");
-addMeme("hej jens", "ldskjnfasd34209p");
-addMeme("hej jens", "ldskjnfasd34209p");
-addMeme("hej jens", "ldskjnfasd34209p");
-addMeme("hej jens", "ldskjnfasd34209p");
+function getMemes() {
+    $.get("/threads/"+tId,function (data) {
+        if(data != "no"){
+            for(i=0;i<data.length;i++)
+            addMeme(data[i]);
+        }
+    })
+}
+getMemes();
